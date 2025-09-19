91c398630d9410e0c8cdebae8f19731562009e15
Mick Letofsky <mletofsky@bitwarden.com>
2025-09-19 18:43:38 +0200

    Fake out

---
+++ bootstrapPlayground/src/app/test.ts
@@ -0,0 +1,27 @@
+// This file is required by karma.conf.js and loads recursively all .spec and framework files
+
+import "zone.js/testing";
+import { getTestBed } from "@angular/core/testing";
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from "@angular/platform-browser-dynamic/testing";
+
+declare const require: {
+  context(
+    path: string,
+    deep?: boolean,
+    pattern?: RegExp
+  ): {
+    keys(): string[];
+    <T>(id: string): T;
+  };
+};
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+
+const context = require.context("./", true, /\.spec\.ts$/);
+context.keys().forEach(context);
